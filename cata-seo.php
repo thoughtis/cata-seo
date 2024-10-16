635ca0b466e314e45c3b157a101804e27c31441f
Jessica Townsend <jessica@thoughtcatalog.com>
2024-10-16 15:57:02 -0400

    initial commit

---
+++ cata-seo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Cata SEO
+ *
+ * @package   Cata\SEO
+ * @author    Thought & Expression Co. <devjobs@thought.is>
+ * @copyright 2019 Thought & Expression Co.
+ * @license   GNU GENERAL PUBLIC LICENSE
+ *
+ * @wordpress-plugin
+ * Plugin Name: Cata SEO
+ * Description: Adds features related to search indexing and structured data.
+ * Author:      Thought & Expression Co. <devjobs@thought.is>
+ * Author URI:  https://thought.is
+ * Version:     0.1.0
+ * License:     GPL v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ */
