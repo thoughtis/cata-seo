ff0a8072c8e22c6f24d44d6a7d46a0b8271028f4
Jessica Townsend <jessica@thoughtcatalog.com>
2024-10-17 17:14:39 -0400

    require structured data class

--- cata-seo.php
+++ cata-seo.php
@@ -16,3 +16,12 @@
  * License:     GPL v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+namespace Cata\SEO;
+
+/**
+ * Structured Data
+ */
+require_once __DIR__ . '/includes/structured-data/class-structured-data.php';
+
+new Structured_Data();
